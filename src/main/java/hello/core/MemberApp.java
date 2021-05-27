80240a078fa68ec4c0f602189d52419edf368f27
minsuk <palt1230@naver.com>
2021-05-27 23:39:38 +0900

    기본 코드 작성 완료

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -6,7 +6,7 @@ import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 public class MemberApp {
     public static void main(String[] args) {
-        
+
         MemberService memberService = new MemberServiceImpl();
 
         Member member = new Member(1L, "memberA", Grade.VIP);
