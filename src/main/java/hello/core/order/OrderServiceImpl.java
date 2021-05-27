b9c626718a4c9af5e27338a90339452992296952
minsuk <palt1230@naver.com>
2021-05-27 23:51:05 +0900

    OCP, DIP 위반

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -8,6 +8,9 @@ import hello.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    // DIP 위반: 할인 정책은 RateDiscountPolicy로 바꾸려면  new fix() interface, 구현 클래스 모두 의존
+    // OCP 위반: 기능 변경하려면 해당 코드를 바꿔야 한다.
     private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
 
     @Override
