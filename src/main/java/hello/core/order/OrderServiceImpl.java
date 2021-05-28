93090d673a09acdb349a765166239e470394a38f
minsuk <palt1230@naver.com>
2021-05-28 23:59:53 +0900

    DIP, DI 적용

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,17 +1,24 @@
 package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import hello.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
 
-    // DIP 위반: 할인 정책은 RateDiscountPolicy로 바꾸려면  new fix() interface, 구현 클래스 모두 의존
-    // OCP 위반: 기능 변경하려면 해당 코드를 바꿔야 한다.
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    //    private final MemberRepository memberRepository = new MemoryMemberRepository();
+//
+//    // DIP 위반: 할인 정책은 RateDiscountPolicy로 바꾸려면  new fix() interface, 구현 클래스 모두 의존
+//    // OCP 위반: 기능 변경하려면 해당 코드를 바꿔야 한다.
+//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy
+            discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
@@ -20,3 +27,4 @@ public class OrderServiceImpl implements OrderService {
         return new Order(memberId, itemName, itemPrice, discountPrice);
     }
 }
+
