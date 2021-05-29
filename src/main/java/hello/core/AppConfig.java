9700991b030e8c5439c78587b0c7658f1de2f88f
minsuk <palt1230@naver.com>
2021-05-29 22:25:25 +0900

    SRP, DIP, OCP 설명 추가

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -2,27 +2,41 @@ package hello.core;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class AppConfig {
+
+    // @Bean이라 적힌 메서드를 모두 호출해서 반환된 객체를 스프링 컨테이너에 등록
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
+
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(
                 memberRepository(),
-                discountPolicy()); }
+                discountPolicy());
+    }
 
+    @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
+// return new FixDiscountPolicy();  쉽게 바꿀수 있다
+        return new RateDiscountPolicy();
     }
 }
